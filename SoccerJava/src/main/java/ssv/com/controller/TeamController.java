581c09c81c6427c86dc143f09a746317ddc6562b
Your NamePTTuan96 <pthanhtuanhp96@gmail.com>
2020-10-28 14:56:57 +0700

    Work with team

---
+++ SoccerJava/src/main/java/ssv/com/controller/TeamController.java
@@ -0,0 +1,27 @@
+package ssv.com.controller;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ssv.com.dto.ResponseQuery;
+import ssv.com.service.TeamService;
+
+@RestController
+@RequestMapping("/api/v1/team/")
+public class TeamController {
+	ModelMapper modelMapper = new ModelMapper();
+
+	@Autowired
+	private TeamService teamService;
+
+	@GetMapping(value = "getTeams")
+	public ResponseQuery<?> getTeams() {
+		if(teamService.getTeams() != null) {
+			return ResponseQuery.success("Connect Success", teamService.getTeams());
+		}
+		return ResponseQuery.faild("Error connect to server", 500);
+	}
+}
